b11e229975eee21d468a733e258264cfd9ac9b8b
Vitalii Rybalko <vilko.a@gmail.com>
2019-06-12 23:04:04 +0300

    fix dice roll
    Signed-off-by: Vitalii Rybalko <vilko.a@gmail.com>

--- src/Game.js
+++ src/Game.js
@@ -4,6 +4,7 @@ const _ = require('lodash');
 const initialState = {
 	isGameFinished: false,
 	isDiceThrowed: false,
+	isDiceRolling: false,
 	points: 0,
 	diceNumber: 6,
 	diceArray: undefined,
@@ -14,6 +15,7 @@ const reducer = (state, action) => {
 	switch (action.type) {
 	case 'roll_dice':
 		return { ...state,
+			isDiceRolling: true,
 			isDiceThrowed: true,
 			diceArray: _.range(state.diceNumber).map((num) => {
 				return Math.ceil(Math.random() * 6)
@@ -27,23 +29,28 @@ const reducer = (state, action) => {
 			selectedDices.add(action.index)
 		}
 		return { ...state, selectedDices }
+	case 'stop_dice_roll':
+		return { ...state, isDiceRolling: false }
 	default:
 		throw new Error(`Not supported action type: ${action.type}`)
 	}
 }
 
-const Cube = ({ number, onClick, isSelected }) => (
-	<div className={`cubeWrapper${isSelected ? ' selected' : ''}`} onClick={onClick}>
-		<div className='cube animated'>
-			<div>{number}</div>
-			<div>&nbsp;</div>
-			<div>&nbsp;</div>
-			<div className='shaded'>{((number + 1) % 6) + 1}</div>
-			<div>&nbsp;</div>
-			<div className='shaded'>{(number - 1) === 0 ? 6 : number - 1}</div>
+const Cube = ({ number, onClick, isDiceRolling }) => {
+	const cubeClassName = `cube${isDiceRolling ? ' animated' : ''}`
+	return (
+		<div className='cubeWrapper' onClick={onClick}>
+			<div className={cubeClassName}>
+				<div>{number}</div>
+				<div>&nbsp;</div>
+				<div>&nbsp;</div>
+				<div className='shaded'>{((number + 1) % 6) + 1}</div>
+				<div>&nbsp;</div>
+				<div className='shaded'>{(number - 1) === 0 ? 6 : number - 1}</div>
+			</div>
 		</div>
-	</div>
-)
+	)
+}
 
 const Game = () => {
 	const [state, dispatch] = useReducer(reducer, initialState)
@@ -51,7 +58,10 @@ const Game = () => {
 
 	return (
 		<div className='container'>
-			<button onClick={() => dispatch({ type: 'roll_dice' })}>Roll Dice</button>
+			<button onClick={() => {
+				dispatch({ type: 'roll_dice' });
+				setTimeout(() => dispatch({ type: 'stop_dice_roll' }), 1000)
+			}}>Roll Dice</button>
 			{state.points > 0 && state.isDiceTrowed && <button>Finish Move</button>}
 			<div className='diceTable'>
 				{state.isDiceThrowed ?
@@ -64,6 +74,7 @@ const Game = () => {
 								key={index}
 								number={number}
 								// isSelected={state.selectedDices.has(index)}
+								isDiceRolling={state.isDiceRolling}
 								onClick={() => dispatch({ type: 'select_dice', index: index })}
 							/>
 						}
